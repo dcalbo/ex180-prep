73138a886567410846cd645bd4d1aae470c615d4
dcalbo <dgcalbo@gmail.com>
2022-12-29 11:11:16 -0300

    Fixed label name

--- scripts/copyToPod.sh
+++ scripts/copyToPod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Copy files  from local dir to pod dir.
 
-oc cp /home/student/dir-to-copy $(oc get pods -l name-infodb -o custom-columns=NAME:.metadata.name | tail -n 1):/tmp
+oc cp /home/student/dir-to-copy $(oc get pods -l name=infodb -o custom-columns=NAME:.metadata.name | tail -n 1):/tmp
 
 echo ". Print files in pod:"
 
-oc exec -it $(oc get pods -l name-infodb -o custom-columns=NAME:.metadata.name | tail -n 1) -- ls -l /tmp
+oc exec -it $(oc get pods -l name=infodb -o custom-columns=NAME:.metadata.name | tail -n 1) -- ls -l /tmp
